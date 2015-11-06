fd1b9ac2e82958db8126b57dbea1f5bc94a324ec
fcaimi <fedeoasi@gmail.com>
2015-11-06 07:26:24 -0600

    Properly serialize DateTimes.

--- app/serialization/JsonSerialization.scala
+++ app/serialization/JsonSerialization.scala
@@ -1,7 +1,14 @@
 package serialization
 
-import org.json4s.DefaultFormats
+import org.joda.time.DateTime
+import org.json4s.JsonAST.JString
+import org.json4s.{CustomSerializer, DefaultFormats}
 
 object JsonSerialization {
-  implicit lazy val formats = DefaultFormats
+  implicit lazy val formats = DefaultFormats + new DateTimeSerializer
+
+  class DateTimeSerializer extends CustomSerializer[DateTime](format => (
+    { case JString(t) => DateTime.parse(t) },
+    { case t: DateTime => JString(t.toString) }
+    ))
 }
