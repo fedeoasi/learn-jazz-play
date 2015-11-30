6926bea5552287fb552e5f0224f80a29d297bfef
fcaimi <fedeoasi@gmail.com>
2015-11-30 17:21:34 -0600

    Add serializer for RatingType.

--- app/serialization/JsonSerialization.scala
+++ app/serialization/JsonSerialization.scala
@@ -4,12 +4,23 @@ import model.Activity
 import org.joda.time.DateTime
 import org.json4s.JsonAST.JString
 import org.json4s.{FieldSerializer, CustomSerializer, DefaultFormats}
+import persistence.general.RatingType
 
 object JsonSerialization {
-  implicit lazy val formats = DefaultFormats + new DateTimeSerializer + new FieldSerializer[Activity]()
+  implicit lazy val formats = DefaultFormats +
+    new DateTimeSerializer +
+    new RatingTypeSerializer +
+    new FieldSerializer[Activity]()
 
   class DateTimeSerializer extends CustomSerializer[DateTime](format => (
     { case JString(t) => DateTime.parse(t) },
     { case t: DateTime => JString(t.toString) }
-    ))
+    )
+  )
+
+  class RatingTypeSerializer extends CustomSerializer[RatingType](format => (
+    { case JString(t) => RatingType(t) },
+    { case t: RatingType => JString(t.discriminator) }
+    )
+  )
 }
